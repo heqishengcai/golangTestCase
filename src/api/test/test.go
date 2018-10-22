1e5a25b7669b4641ff7ce0483e9a2bc855e4a6b5
herui <herui@songguo7.com>
2018-10-22 10:13:13 +0800

    delete some comment

--- src/api/test/test.go
+++ src/api/test/test.go
@@ -13,4 +13,103 @@ import "fmt"
 
 func main() {
 	fmt.Println("test here")
+	//const (
+	//	a = iota
+	//	b
+	//	c
+	//	d = "ha"
+	//	e
+	//	h = iota
+	//	i
+	//	j = 100
+	//	k
+	//)
+	//
+	//fmt.Println(a, b, c, d, e, h, i, j, k)
+
+	//const (
+	//	a = iota
+	//	b
+	//	c
+	//	d
+	//	e = "c"
+	//	f
+	//	j
+	//)
+	//fmt.Println(a, b, c, d, e, f, j)
+
+	//err := errors.New("emit macho dwarf: elf header corrupted")
+	//if err != nil {
+	//	//fmt.Println(err)
+	//}
+
+	//x := [3]int{1, 2, 3}
+	//var x [3]int
+	//x[0] = 1
+	//x[1] = 2
+	//x[2] = 3
+	//fmt.Println(x)
+
+	//doubleArray := [2][5]int{[5]int{1, 2, 3, 4}, [5]int{5, 6, 7, 8}}
+	//doubleArray := [2][6]int{{1, 2}, {4, 5}}
+	//doubleArray := [2][2]int{[2]int{1, 2}, [2]int{3, 4}}
+	//fmt.Println(doubleArray)
+	//Array_a := [10]byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}
+	//Slice_a := Array_a[2:5]
+	//Slice_b := append(Slice_a, "x" byte())
+
+	//fmt.Println(cap(Slice_a))
+	//fmt.Println(len(Slice_a))
+	//
+	//m := make(map[string]string)
+	//m["a"] = "b"
+	//m["a2"] = "b2"
+	//m["a3"] = "b3"
+	//m["a4"] = "b4"
+	//fmt.Println(m)
+	//for index := 10; index > 0; index-- {
+	//	if index == 5 {
+	//		//break // 或者continue
+	//		continue // 或者continue
+	//	}
+	//	fmt.Println(index)
+	//}
+
+	//for k, v := range m {
+	//	fmt.Println(k)
+	//	fmt.Println(v)
+	//}
+
+	//i := 2
+	//switch i {
+	//case 1:
+	//	fmt.Println("i is equal to 1")
+	//case 2, 3:
+	//	fmt.Println("i is equal to 2, 3 or 4")
+	//case 10:
+	//	fmt.Println("i is equal to 10")
+	//default:
+	//	fmt.Println("All I know is that i is an integer")
+	//}
+
+	//integer := 6
+	//switch integer {
+	//case 4:
+	//	fmt.Println("The integer was <= 4")
+	//	fallthrough
+	//case 5:
+	//	fmt.Println("The integer was <= 5")
+	//	fallthrough
+	//case 6:
+	//	fmt.Println("The integer was <= 6")
+	//	fallthrough
+	//case 7:
+	//	fmt.Println("The integer was <= 7")
+	//	fallthrough
+	//case 8:
+	//	fmt.Println("The integer was <= 8")
+	//	fallthrough
+	//default:
+	//	fmt.Println("default case")
+	//}
 }
